b9321ab62c2edb62a41e444218d6150aadb265b3
Uhov <coldboy@inbox.ru>
2022-12-05 19:41:02 +0300

    DZ2

---
+++ lesson_2/z1.py
@@ -0,0 +1,17 @@
+print("Данная программ произведет арифметические действия с двумя числами.")
+a=float(input('Введите первое число: '))
+b=float(input('Введите второе число: '))
+temp=a+b
+print(a,'+',b,'=',temp)
+temp=a-b
+print(a,'-',b,'=',temp)
+temp=a*b
+print(a,'*',b,'=',temp)
+temp=a/b
+print(a,'/',b,'=',temp)
+temp=a**b
+print(a,'**',b,'=',temp)
+temp=a%b
+print(a,'%',b,'=',temp)
+temp=a//b
+print(a,'//',b,'=',temp)
